e6bdc0bd2bb322842d501287b7c6a70fca727905
mauin <marvin.ramin@gmail.com>
2015-10-25 18:07:29 +0100

    adds WelcomeFragment

--- app/src/main/java/com/mtramin/devfestdatabinding/ui/WelcomeActivity.java
+++ app/src/main/java/com/mtramin/devfestdatabinding/ui/WelcomeActivity.java
@@ -4,7 +4,6 @@ import android.content.Intent;
 import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
 
 import com.mtramin.devfestdatabinding.R;
 import com.mtramin.devfestdatabinding.databinding.ActivityWelcomeBinding;
@@ -16,18 +15,12 @@ public class WelcomeActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_welcome);
-
         binding = DataBindingUtil.setContentView(this, R.layout.activity_welcome);
 
-        binding.welcomeText.setText(R.string.welcome_text);
+        getFragmentManager().beginTransaction()
+                .replace(R.id.content, new WelcomeFragment())
+                .commit();
 
-        binding.welcomeButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                showSpeakerList();
-            }
-        });
     }
 
     private void showSpeakerList() {
