c4b3caafca83f3d26aaa5caf5a356c63de6ba664
mauin <marvin.ramin@gmail.com>
2015-11-05 18:26:42 +0100

    updates to gradle 1.5.0-beta1

--- app/src/main/java/com/mtramin/devfestdatabinding/binding/BindingAdapters.java
+++ app/src/main/java/com/mtramin/devfestdatabinding/binding/BindingAdapters.java
@@ -2,11 +2,9 @@ package com.mtramin.devfestdatabinding.binding;
 
 import android.databinding.BindingAdapter;
 import android.graphics.drawable.Drawable;
-import android.support.v4.content.ContextCompat;
 import android.widget.ImageView;
 
 import com.bumptech.glide.Glide;
-import com.mtramin.devfestdatabinding.R;
 
 /**
  * Created by marvi on 10/24/2015.
@@ -15,7 +13,6 @@ public class BindingAdapters {
 
     @BindingAdapter("bind:imageUrl")
     public static void loadImage(ImageView view, String url) {
-        view.setBackgroundColor(ContextCompat.getColor(view.getContext(), R.color.colorPrimary));
         Glide.with(view.getContext())
                 .load(url)
                 .into(view);
@@ -28,14 +25,4 @@ public class BindingAdapters {
                 .error(errorDrawable)
                 .into(view);
     }
-//
-//    @BindingAdapter("android:paddingLeft")
-//    public static void trollPadding(View view, int padding) {
-//        view.setPadding(
-//                view.getPaddingLeft(),
-//                view.getPaddingTop(),
-//                padding,
-//                view.getPaddingBottom()
-//        );
-//    }
 }
